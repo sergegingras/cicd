dbd116fb9d4f7e9eb97881cdf3914151b8aaaabc
sergegingras <s_gingras@hotmail.com>
2022-06-20 18:58:51 -0400

    Initial Commit

---
+++ src/app.py
@@ -0,0 +1,13 @@
+# this s my app file
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "hello world"
+
+
+if __name__  == "__main__":
+    app.run()
+    
